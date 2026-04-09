b27b42299ec9add6fd47c3cd34f4b000c37d4648
Nilanjan Mandal <nilanjanmandal15@gmail.com>
2026-04-09 23:35:31 +0530

    feat: implement multi-client chat server using mpsc channels and threading

--- src/server.rs
+++ src/server.rs
@@ -1,59 +1,109 @@
 use std::{
-    io::{BufRead, BufReader, Read, Write},
-    net::TcpListener,
+    collections::HashMap,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    sync::{
+        Arc,
+        mpsc::{Receiver, Sender, channel},
+    },
+    thread,
 };
+
+struct Client {
+    conn: Arc<TcpStream>,
+}
+
+enum Message {
+    ClientConnected {
+        author: Arc<TcpStream>,
+    },
+    ClientDisconnected {
+        author: Arc<TcpStream>,
+    },
+    NewMessage {
+        author: Arc<TcpStream>,
+        message: Vec<u8>,
+    },
+}
+
+fn server(messages: Receiver<Message>) {
+    let mut clients = HashMap::new();
+
+    loop {
+        let msg = messages.recv().unwrap();
+
+        match msg {
+            Message::ClientConnected { author } => {
+                let addr = author.peer_addr().expect("Unable to get client address");
+                clients.insert(
+                    addr,
+                    Client {
+                        conn: author.clone(),
+                    },
+                );
+            }
+
+            Message::ClientDisconnected { author } => {
+                let addr = author.peer_addr().expect("Unable to get client address");
+                clients.remove_entry(&addr);
+            }
+
+            Message::NewMessage { author, message } => {
+                let author_address = author.peer_addr().expect("Unable to get author address");
+
+                for client in clients.values() {
+                    let client_address = client
+                        .conn
+                        .peer_addr()
+                        .expect("Unable to get client address");
+
+                    if author_address != client_address {
+                        let _ = client.conn.as_ref().write_all(&message);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn client(stream: TcpStream, messages: Sender<Message>) -> Result<(), std::io::Error> {
+    let stream = Arc::new(stream);
+    let mut reader = BufReader::new(stream.as_ref());
+    let _ = messages.send(Message::ClientConnected {
+        author: stream.clone(),
+    });
+
+    loop {
+        let mut line = String::new();
+        let n = reader.read_line(&mut line)?;
+
+        if n == 0 {
+            let _ = messages.send(Message::ClientDisconnected {
+                author: stream.clone(),
+            });
+            break;
+        }
+
+        let _ = messages.send(Message::NewMessage {
+            author: stream.clone(),
+            message: line.into(),
+        });
+    }
+    Ok(())
+}
 
 pub fn start() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("0.0.0.0:8080")?;
 
+    let (message_sender, message_receiver) = channel();
+    thread::spawn(|| server(message_receiver));
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut s) => {
-                let mut reader = BufReader::new(&s);
-                let mut raw_data = String::new();
-
-                loop {
-                    let mut line = String::new();
-                    let n = reader.read_line(&mut line)?;
-
-                    if n == 0 {
-                        break;
-                    }
-
-                    raw_data.push_str(&line);
-
-                    if raw_data.contains("\r\n\r\n") {
-                        break;
-                    }
-                }
-
-                println!("RAW DATA: {raw_data}");
-
-                let headers_end = raw_data.find("\r\n\r\n").unwrap();
-                let headers = &raw_data[..headers_end];
-                let content_length = headers
-                    .lines()
-                    .find(|line| line.to_lowercase().starts_with("content-length"))
-                    .and_then(|line| line.split(": ").nth(1))
-                    .and_then(|v| v.trim().parse::<usize>().ok())
-                    .unwrap_or(0);
-
-                let mut body = raw_data[headers_end + 4..].to_string();
-                while body.len() < content_length {
-                    let mut buff = [0; 100];
-                    let n = reader.read(&mut buff)?;
-
-                    if n == 0 {
-                        break;
-                    }
-
-                    body.push_str(std::str::from_utf8(&buff[..n]).unwrap());
-                }
-                let res = format!("HTTP/1.1 200 OK\r\nContent-Length:{:?}\r\n\r\n{body}", {
-                    body.len()
-                });
-                println!("RES: {res}");
-                let _ = s.write(res.as_bytes())?;
+            Ok(s) => {
+                // let stream = Arc::new(s);
+                let message_sender = message_sender.clone();
+                thread::spawn(|| client(s, message_sender));
             }
             Err(e) => {
                 panic!("{:?}", e);
